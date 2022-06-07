220702f91eb1b49e99945e1151960050d99e48a8
Ubuntu <ubuntu@ip-172-31-42-67.ap-northeast-1.compute.internal>
2022-06-07 23:01:35 +0000

    create rooms controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,13 @@ Rails.application.routes.draw do
   get   'users/account', to: 'users#show'
   get   'users/profile', to: 'users#edit'
   patch 'users/profile', to: 'users#update'
+  
+  get   'rooms/index',   to: 'rooms#index'
+  get   'rooms/new',     to: 'rooms#new'
+  patch 'rooms/new',     to: 'rooms#create'
+  get   'rooms/show',    to: 'rooms#show'
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
