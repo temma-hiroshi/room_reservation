ffeb767b3f4ecc0020e2048fe8477d6439dd757e
Ubuntu <ubuntu@ip-172-31-42-67.ap-northeast-1.compute.internal>
2022-06-10 00:39:10 +0000

    create Room model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_29_113415) do
+ActiveRecord::Schema.define(version: 2022_06_10_003603) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.text "introduction"
+    t.integer "price"
+    t.string "address"
+    t.string "image"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
