59dba7e8f9ee5f584470b8753099d266db641993
temma hiroshi <temmahiroshi.engineer@gmail.com>
2022-06-12 06:25:32 +0000

    modified room model validation

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,11 +1,11 @@
 class Room < ApplicationRecord
   belongs_to :user
   
-  # validates :name,          presence: true
-#   validates :introduction,  presence: true
-#   validates :price,         presence: true
-#   validates :address,       presence: true
-#   validates :image,         presence: true
+  validates :name,          presence: true
+  validates :introduction,  presence: true
+  validates :price,         presence: true
+  validates :address,       presence: true
+  validates :image,         presence: true
 
   mount_uploader :image, ImageUploader
 
